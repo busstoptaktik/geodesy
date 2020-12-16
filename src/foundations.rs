ab26c862cc65b442c8d01516bc43bf8bf63529f3
Thomas Knudsen <knudsen.thomas@gmail.com>
2020-12-16 18:49:40 +0100

    mere pudsning af ellps

--- src/foundations.rs
+++ src/foundations.rs
@@ -16,6 +16,8 @@ impl DMS {
     }
 }
 
+
+
 #[derive(Clone, Copy, Debug)]
 pub struct Ellipsoid {
     pub name: &'static str,
@@ -23,7 +25,41 @@ pub struct Ellipsoid {
     pub f: f64,
 }
 
+
+// A hashmap indexed by the ellipsoid name would be better, but Rust cannot
+// statically initialize a hashmap, so we put the name into the struct and
+// use a static array instead.
+static ELLIPSOIDS: [Ellipsoid; 5] =  [
+    Ellipsoid {name: "GRS80"  ,  a: 6378137.0,   f: 1./298.257222100882711243},
+    Ellipsoid {name: "intl"   ,  a: 6378388.0,   f: 1./297.},
+    Ellipsoid {name: "Helmert",  a: 6378200.0,   f: 1./298.3},
+    Ellipsoid {name: "clrk66" ,  a: 6378206.4,   f: 1./294.9786982},
+    Ellipsoid {name: "clrk80" ,  a: 6378249.145, f: 1./293.465}
+];
+
+
+pub fn ellipsoid(name: &str) -> Ellipsoid {
+    for e in ELLIPSOIDS.iter() {
+        if e.name == name {
+            return *e;
+        }
+    }
+    ELLIPSOIDS[0]
+}
+
+
 impl Ellipsoid {
+
+    pub fn named(name: &str) -> Ellipsoid {
+        for e in ELLIPSOIDS.iter() {
+            if e.name == name {
+                return *e;
+            }
+        }
+        ELLIPSOIDS[0]
+    }
+
+
     pub fn eccentricity_squared(&self) -> f64 {
         self.f*(2_f64 - self.f)
     }
@@ -46,13 +82,48 @@ impl Ellipsoid {
         self.a * (1.0 - self.f)
     }
 
+    pub fn semimajor_axis(&self) -> f64 {
+        self.a
+    }
+
+    pub fn flattening(&self) -> f64 {
+        self.f
+    }
+
     pub fn prime_vertical_radius_of_curvature(&self, latitude: f64) -> f64 {
-        let sinlat = latitude.sin();
         if self.f == 0.0 {
             return self.a;
         }
-        self.a / (1.0 - sinlat * sinlat * self.eccentricity_squared()).sqrt()
+        self.a / (1.0 - latitude.sin().powi(2) * self.eccentricity_squared()).sqrt()
     }
+
+    pub fn meridian_radius_of_curvature(&self, latitude: f64) -> f64 {
+        if self.f == 0.0 {
+            return self.a;
+        }
+        let num = self.a * (1.0 - self.eccentricity_squared());
+        let denom = (1.0 - latitude.sin().powi(2) * self.eccentricity_squared()).powf(1.5);
+        num / denom
+    }
+
+    /// Geographic latitude to geocentric latitude
+    /// (or vice versa if `forward` is `false`).
+    pub fn geocentric_latitude(&self, latitude: f64, forward: bool) -> f64 {
+        if forward {
+            return ((1.0 - self.f * (2.0 - self.f)) * latitude.tan()).atan();
+        }
+        (latitude.tan() / (1.0 -self.eccentricity_squared())).atan()
+    }
+
+    /// Geographic latitude to reduced latitude
+    /// (or vice versa if `forward` is  `false`).
+    pub fn reduced_latitude(&self, latitude: f64, forward: bool) -> f64 {
+        if forward {
+            return ((1.0 - self.f) * latitude.tan()).atan();
+        }
+        (latitude.tan() / (1.0 - self.f)).atan()
+    }
+
 
     #[allow(non_snake_case)]
     pub fn cartesian(&self, geographic: &CoordinateTuple) -> CoordinateTuple {
@@ -119,58 +190,6 @@ impl Ellipsoid {
     }
 }
 
-
-// A hashmap indexed by the ellipsoid name would be better, but Rust cannot
-// statically initialize a hashmap, so we put the name into the struct and
-// use a static array instead.
-static ELLIPSOIDS: [Ellipsoid; 5] =  [
-    Ellipsoid {name: "GRS80"  ,  a: 6378137.0,   f: 1./298.257222100882711243},
-    Ellipsoid {name: "intl"   ,  a: 6378388.0,   f: 1./297.},
-    Ellipsoid {name: "Helmert",  a: 6378200.0,   f: 1./298.3},
-    Ellipsoid {name: "clrk66" ,  a: 6378206.4,   f: 1./294.9786982},
-    Ellipsoid {name: "clrk80" ,  a: 6378249.145, f: 1./293.465}
-];
-
-pub fn ellipsoid(name: &str) -> Ellipsoid {
-    for e in ELLIPSOIDS.iter() {
-        if e.name == name {
-            return *e;
-        }
-    }
-    ELLIPSOIDS[0]
-}
-
-
-/// Misc. auxiliary latitudes
-pub trait Latitude {
-    /// Geographic latitude to geocentric latitude
-    /// or vice versa if `forward` is set to `false`.
-    fn geocentric(&self, f: f64, forward: bool) -> f64;
-
-    /// Geographic latitude to reduced latitude
-    /// or vice versa if `forward` is set to `false`.
-    fn reduced(&self, f: f64, forward: bool) -> f64;
-}
-
-
-impl Latitude for f64 {
-    fn geocentric(&self, f: f64, forward: bool) -> f64 {
-        // Geographic to geocentric?
-        if forward {
-            return ((1.0 - f * (2.0 - f)) * self.tan()).atan();
-        }
-        (self.tan() / (1.0 - f * (2.0 - f))).atan()
-    }
-
-    fn reduced(&self, f: f64, forward: bool) -> f64 {
-        // Geographic to reduced?
-        if forward {
-            return ((1.0 - f) * self.tan()).atan();
-        }
-        (self.tan() / (1.0 - f)).atan()
-    }
-}
-
 mod tests {
     #[test]
     fn test_dms() {
@@ -184,11 +203,12 @@ mod tests {
 
     #[test]
     fn test_ellipsoid() {
-        let ellps = super::ellipsoid("GRS80");
+        use super::Ellipsoid;
+        let ellps = Ellipsoid::named("GRS80");
         assert_eq!(ellps.a, 6378137.0);
         assert_eq!(ellps.f, 1. / 298.257222100882711243);
 
-        let ellps = super::ellipsoid("intl");
+        let ellps = Ellipsoid::named("intl");
         assert_eq!(ellps.f, 1. / 297.);
 
         let geo = super::CoordinateTuple{0: 12_f64.to_radians(), 1: 55_f64.to_radians(), 2: 100.0, 3: 0.};
@@ -197,58 +217,15 @@ mod tests {
         assert!((geo.0-geo2.0) < 1.0e-12);
         assert!((geo.1-geo2.1) < 1.0e-12);
 
-    }
+        // Roundtrip geocentric latitude
+        let lat = 55_f64.to_radians();
+        let lat2 = ellps.geocentric_latitude(ellps.geocentric_latitude(lat, true), false);
+        assert!((lat-lat2) < 1.0e-12);
 
-    #[test]
-    fn test_latitude() {
-        use super::Latitude;
-        let ellps = super::ellipsoid("GRS80");
-        let phi = 60.0_f64.to_radians();
-        let theta = phi.geocentric(ellps.f, true);
-        let phi2 = theta.geocentric(ellps.f, false);
-        assert!((phi - phi2).abs() < 1.0e-10);
-        let theta2 = phi2.geocentric(ellps.f, true);
-        assert!((theta - theta2).abs() < 1.0e-10);
+        // Roundtrip reduced latitude
+        let lat = 55_f64.to_radians();
+        let lat2 = ellps.reduced_latitude(ellps.reduced_latitude(lat, true), false);
+        assert!((lat-lat2) < 1.0e-12);
 
-        let beta = phi.reduced(ellps.f, true);
-        let phi2 = beta.reduced(ellps.f, false);
-        assert!((phi - phi2).abs() < 1.0e-10);
     }
 }
-
-/*
-# Abbreviations used
-#
-# In general, for code readability, concepts are spelled out fully in
-# identifiers (i.e. constants, variables, functions, classes).
-#
-# In a few very common cases, it makes sense to maintain short forms.
-# These cases are limited to:
-#
-# lat:       latitude
-# lon:       longitude
-# f:         flattening
-# rf:        inverse flattening
-# e:         eccentricity
-# e2:        eccentricity squared
-# deg:       degree(s)
-# rad:       radian(s)
-#
-
-def torad(deg):
-    return pi*deg/180.
-def todeg(rad):
-    return 180*rad/pi
-def dms2dd(dms):
-    return (dms[2]/60. + dms[1])/60. + dms[0]
-
-
-def geocentric_lat(geographic_lat, f):
-    return atan((1 - f*(2 - f))*tan(geographic_lat))
-
-def geographic_lat(geocentric_lat, f):
-    return atan(tan(geocentric_lat) / (1 - f*(2 - f)))
-
-def eccentricity_squared(f):
-    return f*(2 - f)
-*/
