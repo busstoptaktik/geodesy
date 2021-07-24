2ca3d8ce4b9c5a647dfc9b490205e1a1527e96f6
Thomas Knudsen <knudsen.thomas@gmail.com>
2021-07-25 00:11:11 +0200

    Header for Gas done

--- src/gas.rs
+++ src/gas.rs
@@ -3,29 +3,34 @@ use crate::CoordinateTuple;
 #[derive(Debug, Default)]
 pub struct Gas {
     header: GasHeader,
-    v: Vec<f64>,
+    grid: Vec<f64>,
 }
 
 #[derive(Debug, Default)]
 pub struct GasHeader {
     bbox: (CoordinateTuple, CoordinateTuple),
     delta: CoordinateTuple,
-    dim: (usize, usize, usize, usize),
+    dist: CoordinateTuple,
+    dim: (usize, usize, usize, usize, usize),
+    skip: usize,
+    size: usize
 }
 
 impl GasHeader {
     pub fn new() -> GasHeader {
+        let blank = CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN);
         GasHeader{
-            bbox: (
-                CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN),
-                CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN)
-            ),
-            delta: CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN),
-            dim: (1_usize, 1_usize, 1_usize, 1_usize)
+            bbox: (blank, blank),
+            delta: blank,
+            dist: blank,
+            dim: (1, 1, 1, 1, 1),
+            skip: 0,
+            size: 0
         }
     }
 
     pub fn update(&mut self, text: &str) -> bool {
+        // Parse the input line as a record type id followed by 1-8 numbers
         let mut parts: Vec<&str> = text.split_whitespace().collect();
         let n = parts.len() - 1;
         if n < 1 || n > 8 {
@@ -38,11 +43,12 @@ impl GasHeader {
             b.push(e.parse().unwrap_or(std::f64::NAN))
         }
 
+        // The Bounding Box consists of 2 CoordinateTuples, lower left and upper right
         if element=="bbox" {
             if  n==4 {  // 2D
                 self.bbox = (
-                    CoordinateTuple(b[1],b[0],b[7],b[7]),
-                    CoordinateTuple(b[3],b[2],b[7],b[7])
+                    CoordinateTuple(b[1],b[0],b[7],b[7]),  // LL
+                    CoordinateTuple(b[3],b[2],b[7],b[7])   // UR
                 );
                 return true;
             }
@@ -63,15 +69,45 @@ impl GasHeader {
             return false
         }
 
-        if element=="delta" {
+        if element=="cols" || element=="columns" {
+            self.dim.0 = b[0] as usize;
             return true;
         }
 
+        if element=="rows" {
+            self.dim.1 = b[0] as usize;
+            return true;
+        }
 
+        if element=="levels" {
+            self.dim.2 = b[0] as usize;
+            return true;
+        }
+
+        if element=="steps" {
+            self.dim.3 = b[0] as usize;
+            return true;
+        }
+
+        if element=="skip" {
+            self.skip = b[0] as usize;
+            return true;
+        }
+
+        if element=="bands" || element=="channels" {
+            self.dim.4 = b[0] as usize;
+            return true;
+        }
 
         false
     }
-    pub fn is_complete(&self) -> bool {
+
+    pub fn finalize(&mut self) -> bool {
+        self.delta.0 = (self.bbox.1.0 - self.bbox.0.0) / self.dim.0 as f64;
+        self.delta.1 = (self.bbox.1.1 - self.bbox.0.1) / self.dim.1 as f64;
+        self.delta.2 = (self.bbox.1.2 - self.bbox.0.2) / self.dim.2 as f64;
+        self.delta.3 = (self.bbox.1.3 - self.bbox.0.3) / self.dim.3 as f64;
+        self.size = self.dim.0 * self.dim.1 * self.dim.2 * self.dim.3 *self.dim.4;
         true
     }
 }
@@ -85,10 +121,12 @@ enum GasReaderState {Pre, Header, Grid, Post}
 
 impl Gas {
     pub fn new(name: &str) -> Result<Gas, String> {
+        let mut grid: Vec<f64> = vec![];
+        let mut header = GasHeader::new();
+
+        // Loop over all lines in the file. Select actions through state changes
         if let Ok(lines) = Gas::read_lines(name) {
             let mut state = GasReaderState::Pre;
-            let _values: Vec<f64> = vec![];
-            let mut header = GasHeader::new();
 
             for line in lines {
                 if let Ok(text) = line {
@@ -97,21 +135,47 @@ impl Gas {
                     if text.is_empty() {
                         continue;
                     }
-                    println!("{}", text);
                     match state {
-                        GasReaderState::Pre =>
-                            if text=="header" {state = GasReaderState::Header;},
-                        GasReaderState::Header =>
+
+                        // Skip the prose in the file preamble
+                        GasReaderState::Pre => {
+                            if text=="header" {state = GasReaderState::Header}
+                        },
+
+                        // Read and finalize the header
+                        GasReaderState::Header => {
                             if text=="grid" {
-                                if header.is_complete() {
-                                    state = GasReaderState::Grid;
-                                    continue;
+                                if !header.finalize() {
+                                    return Err("Bad header".to_string());
                                 }
-                                return Err("Incomplete grid header".to_string());
+                                state = GasReaderState::Grid;
                             }
-                            else {header.update(text); println!("{:?}", header)},
-                        GasReaderState::Grid => break,
-                        GasReaderState::Post => break,
+                            else if !header.update(text) {
+                                return Err("Bad header: ".to_string() + text);
+                            }
+                        },
+
+                        // Read the grid
+                        GasReaderState::Grid => {
+                            let mut parts: Vec<&str> = text.split_whitespace().collect();
+                            let n = parts.len();
+                            if n < header.skip + header.dim.4 {
+                                return Err("Bad format: ".to_string() + text)
+                            }
+                            parts.drain(0..header.skip);
+                            parts.truncate(header.dim.4);
+                            for v in parts {
+                                grid.push(v.parse().unwrap_or(std::f64::NAN));
+                            }
+                            if grid.len() >= header.size {
+                                state = GasReaderState::Post;
+                            }
+                        },
+
+                        // Tell the world we're done - skip remaining parts (if any)
+                        GasReaderState::Post => {
+                            break
+                        }
                     }
                     continue;
                 }
@@ -120,8 +184,7 @@ impl Gas {
                 return Err(String::from("Incomplete file"));
             }
         }
-        Err(String::from("Not done"))
-        //Ok(Gas {})
+        Ok(Gas{header, grid})
     }
 
     // Returns an Iterator to the Reader of the lines of the file.
@@ -134,6 +197,7 @@ impl Gas {
     }
 
     pub fn value(_at: CoordinateTuple) -> CoordinateTuple {
+        // Grid interpolation!
         todo!()
     }
 }
