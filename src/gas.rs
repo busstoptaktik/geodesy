7ca2916415ebe8a8cd4ea35a6fc9f3dea62fac8a
Thomas Knudsen <knudsen.thomas@gmail.com>
2021-07-24 19:00:09 +0200

    Header for Gas: bbox

--- src/gas.rs
+++ src/gas.rs
@@ -2,10 +2,78 @@ use crate::CoordinateTuple;
 
 #[derive(Debug, Default)]
 pub struct Gas {
+    header: GasHeader,
+    v: Vec<f64>,
+}
+
+#[derive(Debug, Default)]
+pub struct GasHeader {
     bbox: (CoordinateTuple, CoordinateTuple),
     delta: CoordinateTuple,
     dim: (usize, usize, usize, usize),
-    v: Vec<f64>,
+}
+
+impl GasHeader {
+    pub fn new() -> GasHeader {
+        GasHeader{
+            bbox: (
+                CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN),
+                CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN)
+            ),
+            delta: CoordinateTuple(std::f64::NAN,std::f64::NAN,std::f64::NAN,std::f64::NAN),
+            dim: (1_usize, 1_usize, 1_usize, 1_usize)
+        }
+    }
+
+    pub fn update(&mut self, text: &str) -> bool {
+        let mut parts: Vec<&str> = text.split_whitespace().collect();
+        let n = parts.len() - 1;
+        if n < 1 || n > 8 {
+            return false
+        }
+        let element = parts.remove(0);
+        parts.extend(["NAN"; 8]);
+        let mut b: Vec<f64> = vec![];
+        for e in parts {
+            b.push(e.parse().unwrap_or(std::f64::NAN))
+        }
+
+        if element=="bbox" {
+            if  n==4 {  // 2D
+                self.bbox = (
+                    CoordinateTuple(b[1],b[0],b[7],b[7]),
+                    CoordinateTuple(b[3],b[2],b[7],b[7])
+                );
+                return true;
+            }
+            if  n==6 {  // 3D
+                self.bbox = (
+                    CoordinateTuple(b[2],b[1],b[0],b[7]),
+                    CoordinateTuple(b[5],b[4],b[3],b[7])
+                );
+                return true;
+            }
+            if  n==8 { // 4D
+                self.bbox = (
+                    CoordinateTuple(b[3],b[2],b[1],b[0]),
+                    CoordinateTuple(b[3],b[2],b[1],b[0])
+                );
+                return true;
+            }
+            return false
+        }
+
+        if element=="delta" {
+            return true;
+        }
+
+
+
+        false
+    }
+    pub fn is_complete(&self) -> bool {
+        true
+    }
 }
 
 use std::fs::File;
@@ -19,19 +87,29 @@ impl Gas {
     pub fn new(name: &str) -> Result<Gas, String> {
         if let Ok(lines) = Gas::read_lines(name) {
             let mut state = GasReaderState::Pre;
-            let values: Vec<f64> = vec![];
+            let _values: Vec<f64> = vec![];
+            let mut header = GasHeader::new();
 
             for line in lines {
-                if let Ok(txt) = line {
-                    let parts: Vec<&str> = txt.split('#').collect();
+                if let Ok(text) = line {
+                    let parts: Vec<&str> = text.split('#').collect();
                     let text = parts[0].trim();
                     if text.is_empty() {
                         continue;
                     }
                     println!("{}", text);
                     match state {
-                        GasReaderState::Pre => if txt=="header" {state = GasReaderState::Header;},
-                        GasReaderState::Header => break,
+                        GasReaderState::Pre =>
+                            if text=="header" {state = GasReaderState::Header;},
+                        GasReaderState::Header =>
+                            if text=="grid" {
+                                if header.is_complete() {
+                                    state = GasReaderState::Grid;
+                                    continue;
+                                }
+                                return Err("Incomplete grid header".to_string());
+                            }
+                            else {header.update(text); println!("{:?}", header)},
                         GasReaderState::Grid => break,
                         GasReaderState::Post => break,
                     }
