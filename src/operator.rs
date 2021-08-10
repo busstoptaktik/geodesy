ec37f5242c03320ced55681192c34c4a60a07298
Thomas Knudsen <knudsen.thomas@gmail.com>
2021-08-10 14:41:07 +0200

    Move gys_to_yaml from context to operator

--- src/operator.rs
+++ src/operator.rs
@@ -25,12 +25,7 @@ impl Operator {
     /// assert!((operands[0][0].to_degrees() - 12.).abs() < 1.0e-10);
     /// ```
     pub fn new(definition: &str, ctx: &mut Context) -> Option<Operator> {
-        // If it is a macro without arguments, we append an empty argument list
-        let mut definition = definition.trim().to_string();
-        let first_word: String = definition.split(':').take(1).collect();
-        if definition.len() == first_word.len() {
-            definition += ": {}";
-        }
+        let definition = gys_to_yaml(definition);
 
         let mut oa = OperatorArgs::new();
         oa.populate(&definition, "");
@@ -150,6 +145,144 @@ pub trait OperatorCore {
     //fn right(&self) -> CoordType;
 }
 
+
+/// Convert "Ghastly YAML Shorthand" to YAML
+fn gys_to_yaml(gys: &str) -> String {
+    // TODO: remove comments, blank lines and leading/trailing whitespace
+
+    let lines = gys.lines();
+    let mut s = Vec::new();
+    for line in lines {
+        if line.trim().starts_with('#') {
+            continue;
+        }
+        s.push(line);
+    }
+    let gys = s.join("\n").trim().to_string();
+
+    // Appears to be YAML already - do nothing!
+    if !is_gys(&gys) {
+        return gys;
+    }
+
+    // Strip off superfluous GYS indicators
+    let gys = gys.trim_matches('|');
+    let gys = gys.trim_matches('[');
+    let gys = gys.trim_matches(']');
+
+    let mut yaml = String::new();
+    let mut indent = "";
+    let steps: Vec<&str> = gys.split('|').collect();
+    let nsteps = steps.len();
+    if nsteps > 1 {
+        yaml += "pipeline_from_gys: {\n  steps: [\n";
+        indent = "    ";
+    }
+    for step in steps {
+        let mut elements: Vec<&str> = step.split_whitespace().collect();
+        let n = elements.len();
+        if n == 0 {
+            return String::from("Error: Empty step!");
+        }
+
+        // First the operator name
+        yaml += indent;
+        yaml += elements[0];
+        yaml += ":";
+
+        // linebreaks after the first step
+        indent = ",\n    ";
+        // No args? Then insert an empty argument list
+        if n == 1 {
+            yaml += " {}";
+            continue;
+        }
+
+        // Handle args
+        yaml += " {";
+
+        for i in 1..n {
+            // We constructed a key-value par in last iteration?
+            if elements[i].is_empty() {
+                continue;
+            }
+            let e = elements[i].to_string();
+            if e.ends_with(':') {
+                if i == n - 1 {
+                    return String::from("Missing value for key '") + &e + "'";
+                }
+                yaml += &e;
+                yaml += " ";
+                yaml += elements[i + 1];
+                if i + 2 < n {
+                    yaml += ", ";
+                }
+                elements[i + 1] = "";
+                continue;
+            };
+
+            // Ultra compact notation: key:value, no whitespace
+            if e.contains(':') {
+                yaml += &e.replace(":", ": ");
+                if i + 1 < n {
+                    yaml += ", ";
+                }
+                continue;
+            }
+
+            // Key with no value? provide "true"
+            yaml += &e;
+            yaml += ": true";
+            if i + 1 < n {
+                yaml += ", ";
+            }
+        }
+        yaml += "}";
+    }
+
+    if nsteps > 1 {
+        yaml += "\n  ]\n}";
+    }
+
+    yaml
+}
+
+// True if a str appears to be in GYS format
+fn is_gys(gys: &str) -> bool {
+    // GYS if contains a whitespace-wrapped pipe
+    if gys.contains(" | ") {
+        return true;
+    }
+
+    // GYS if starting or ending with an empty step
+    if gys.starts_with('|') {
+        return true;
+    }
+    if gys.ends_with('|') {
+        return true;
+    }
+
+    // GYS if wrapped in square brackets: [gys]. Note that
+    // we cannot merge these two ifs without damaging the
+    // following test for "no trailing colon"
+    if gys.starts_with('[') {
+        return gys.ends_with(']');
+    }
+    if gys.ends_with(']') {
+        return gys.starts_with('[');
+    }
+
+    // GYS if no trailing colon on first token
+    if !gys.split_whitespace().next().unwrap_or_default().ends_with(':') {
+        return true
+    }
+
+    // Otherwise not a GYS - hopefully it's YAML then!
+    false
+}
+
+
+
 mod cart;
 mod fit;
 mod helmert;
@@ -174,6 +307,11 @@ pub(crate) fn operator_factory(
     }
 
     // Look for macros defined by files in the current working directory
+    if let Ok(definition) = std::fs::read_to_string(args.name.clone() + ".gys") {
+        let definition = gys_to_yaml(&definition);
+        let mut moreargs = args.spawn(&definition);
+        return operator_factory(&mut moreargs, ctx, recursions + 1);
+    }
     if let Ok(definition) = std::fs::read_to_string(args.name.clone() + ".yml") {
         let mut moreargs = args.spawn(&definition);
         return operator_factory(&mut moreargs, ctx, recursions + 1);
@@ -503,4 +641,55 @@ mod tests {
         let _aha = ctx.fwd(op, operands.as_mut());
         assert_eq!(operands[0][0], 42.);
     }
+
+    #[test]
+    fn gys() {
+        use crate::Context;
+        use super::is_gys;
+        use crate::CoordinateTuple as C;
+
+        // A pipeline in YAML
+        let pipeline = "ed50_etrs89: {
+            # with cucumbers
+            steps: [
+                cart: {ellps: intl},
+                helmert: {x: -87, y: -96, z: -120},
+                cart: {inv: true, ellps: GRS80}
+            ]
+        }";
+
+        // Same pipeline in Ghastly YAML Shorthand (GYS)
+        let gys = "cart ellps: intl | helmert x:-87 y:-96 z:-120 | cart inv ellps:GRS80";
+
+        let mut ctx = Context::new();
+
+        // Check that the GYS syntactical indicators trigger
+        assert!(is_gys("[cart]"));
+        assert!(is_gys("|cart|"));
+        assert!(is_gys("|cart"));
+        assert!(is_gys("cart|"));
+        assert!(!is_gys("[cart"));
+        assert!(!is_gys("cart]"));
+
+        // Check that GYS instantiates exactly as the corresponding YAML
+        let op_yaml = ctx.operation(pipeline).unwrap();
+        let op_gys = ctx.operation(gys).unwrap();
+
+        let copenhagen = C::geo(55., 12., 0., 0.);
+        let stockholm = C::geo(59., 18., 0., 0.);
+        let mut yaml_data = [copenhagen, stockholm];
+        let mut gys_data = [copenhagen, stockholm];
+
+        ctx.fwd(op_yaml, &mut yaml_data);
+        ctx.fwd(op_gys, &mut gys_data);
+
+        C::geo_all(&mut yaml_data);
+        C::geo_all(&mut gys_data);
+
+        // We assert that the difference is exactly zero, since the operations
+        // should be identical. But float equality comparisons are frowned at...
+        assert!(yaml_data[0].hypot3(&gys_data[0]) < 1e-30);
+        assert!(yaml_data[1].hypot3(&gys_data[1]) < 1e-30);
+    }
+
 }
