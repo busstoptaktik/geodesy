0ec134f294a493639c6c8b23ccd4323188882102
Thomas Knudsen <knudsen.thomas@gmail.com>
2022-02-01 09:17:12 +0100

    Improved testing for Helmert

--- src/bin/geod/inner_op/helmert.rs
+++ src/bin/geod/inner_op/helmert.rs
@@ -1,19 +1,20 @@
 #![allow(non_snake_case)]
-
 /// The Helmert transform performs reference frame shifts. It operates in the 3D cartesian
 /// space, transforming 3D cartesian coordinates between static and/or dynamic reference
 /// frames, e.g. from global reference frames to local static frames.
 ///
 /// While generally also applicable to 2D coordinates, this functionality is not yet
 /// implemented.
-
-
-
 use super::*;
 
-// ----- F O R W A R D --------------------------------------------------------------
+// ----- C O M M O N -------------------------------------------------------------------
 
-fn helmert_fwd(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -> usize {
+fn helmert_common(
+    op: &Op,
+    _prv: &dyn Provider,
+    operands: &mut [CoordinateTuple],
+    direction: Direction,
+) -> usize {
     // Translation, Rotation, Scale
     let T = op.params.series("T").unwrap();
     let R = op.params.series("R").unwrap();
@@ -31,14 +32,13 @@ fn helmert_fwd(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -
     // Predicates
     let rotated = op.params.boolean("rotated");
     let dynamic = op.params.boolean("dynamic");
-    let fixed_t = op.params.boolean("fixed_t");
+    let fixed_t = op.params.boolean("fixed_time");
     let exact = op.params.boolean("exact");
     let position_vector = op.params.boolean("position_vector");
 
     let epoch = op.params.real("t_epoch").unwrap_or(0.);
 
-    let mut TT = Vec::from(T);
-    assert_eq!(TT.len(), 3);
+    let mut TT = [T[0], T[1], T[2]];
     let mut SS = S;
 
     let mut prev_t = std::f64::NAN;
@@ -55,95 +55,41 @@ fn helmert_fwd(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -
                 TT[1] += dt * DT[1];
                 TT[2] += dt * DT[2];
                 if rotated {
-                    let mut RR = Vec::from(R);
-                    assert_eq!(RR.len(), 3);
-
-                    RR[0] += dt * DR[0];
-                    RR[1] += dt * DR[1];
-                    RR[2] += dt * DR[2];
-                    ROT = rotation_matrix(RR, exact, position_vector);
-                    dbg!(ROT);
+                    let RR = [
+                        R[0] + dt * DR[0],
+                        R[1] + dt * DR[1],
+                        R[2] + dt * DR[2]
+                    ];
+                    ROT = rotation_matrix(&RR, exact, position_vector);
                 }
                 SS = S + dt * DS;
             }
         }
 
-        if rotated {
-            // Rotate
-            let x = c[0] * ROT[0][0] + c[1] * ROT[0][1] + c[2] * ROT[0][2];
-            let y = c[0] * ROT[1][0] + c[1] * ROT[1][1] + c[2] * ROT[1][2];
-            let z = c[0] * ROT[2][0] + c[1] * ROT[2][1] + c[2] * ROT[2][2];
+        // ----- Forward direction -----
 
-            // scale and offset
-            c[0] = SS * x + TT[0];
-            c[1] = SS * y + TT[1];
-            c[2] = SS * z + TT[2];
+        if direction == Direction::Fwd {
+            if rotated {
+                // Rotate
+                let x = c[0] * ROT[0][0] + c[1] * ROT[0][1] + c[2] * ROT[0][2];
+                let y = c[0] * ROT[1][0] + c[1] * ROT[1][1] + c[2] * ROT[1][2];
+                let z = c[0] * ROT[2][0] + c[1] * ROT[2][1] + c[2] * ROT[2][2];
+
+                // scale and offset
+                c[0] = SS * x + TT[0];
+                c[1] = SS * y + TT[1];
+                c[2] = SS * z + TT[2];
+                continue;
+            }
+
+            // scale and offset without rotation
+            c[0] = SS * c[0] + TT[0];
+            c[1] = SS * c[1] + TT[1];
+            c[2] = SS * c[2] + TT[2];
             continue;
         }
 
-        // scale and offset without rotation
-        c[0] = SS * c[0] + TT[0];
-        c[1] = SS * c[1] + TT[1];
-        c[2] = SS * c[2] + TT[2];
-    }
-    n
-}
-
-// ----- I N V E R S E --------------------------------------------------------------
-
-fn helmert_inv(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -> usize {
-    // Translation, Rotation, Scale
-    let T = op.params.series("T").unwrap();
-    let R = op.params.series("R").unwrap();
-    let S = op.params.real("S").unwrap();
-
-    // ... and their time evolution
-    let DT = op.params.series("DT").unwrap();
-    let DR = op.params.series("DR").unwrap();
-    let DS = op.params.real("DS").unwrap();
-
-    // The precomputed rotation matrix
-    let M = op.params.series("ROTFLAT").unwrap();
-    let mut ROT = [[M[0], M[1], M[2]], [M[3], M[4], M[5]], [M[6], M[7], M[8]]];
-
-    // Predicates
-    let rotated = op.params.boolean("rotated");
-    let dynamic = op.params.boolean("dynamic");
-    let fixed_t = op.params.boolean("fixed_t");
-    let exact = op.params.boolean("exact");
-    let position_vector = op.params.boolean("position_vector");
-
-    let epoch = op.params.real("t_epoch").unwrap_or(0.);
-
-    let mut TT = Vec::from(T);
-    assert_eq!(TT.len(), 3);
-    let mut SS = S;
-
-    let mut prev_t = std::f64::NAN;
-    let n = operands.len();
-    for c in operands {
-        // Time varying case?
-        if dynamic && !fixed_t {
-            // Necessary to update parameters?
-            #[allow(clippy::float_cmp)]
-            if c[3] != prev_t {
-                prev_t = c[3];
-                let dt = c[3] - epoch;
-                TT[0] += dt * DT[0];
-                TT[1] += dt * DT[1];
-                TT[2] += dt * DT[2];
-                if rotated {
-                    let mut RR = Vec::from(R);
-                    assert_eq!(RR.len(), 3);
-
-                    RR[0] += dt * DR[0];
-                    RR[1] += dt * DR[1];
-                    RR[2] += dt * DR[2];
-                    ROT = rotation_matrix(RR, exact, position_vector);
-                }
-                SS = S + dt * DS;
-            }
-        }
+        // ----- Inverse direction -----
 
         // Deoffset and unscale
         let x = (c[0] - TT[0]) / SS;
@@ -162,6 +108,18 @@ fn helmert_inv(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -
         }
     }
     n
+}
+
+// ----- F O R W A R D --------------------------------------------------------------
+
+fn helmert_fwd(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -> usize {
+    return helmert_common(op, _prv, operands, Direction::Fwd);
+}
+
+// ----- I N V E R S E --------------------------------------------------------------
+
+fn helmert_inv(op: &Op, _prv: &dyn Provider, operands: &mut [CoordinateTuple]) -> usize {
+    return helmert_common(op, _prv, operands, Direction::Inv);
 }
 
 // ----- C O N S T R U C T O R ------------------------------------------------------
@@ -194,7 +152,6 @@ pub const GAMUT: [OpParameter; 19] = [
     OpParameter::Text { key: "convention", default: Some("") },
     OpParameter::Flag { key: "exact" },
 
-
     // Scale and its time evoution
     OpParameter::Real { key: "s",  default: Some(0f64) },
     OpParameter::Real { key: "ds", default: Some(0f64) },  // TODO: scale by 1e-6
@@ -202,7 +159,7 @@ pub const GAMUT: [OpParameter; 19] = [
     // Epoch - "beginning of time for this transformation"
     OpParameter::Real { key: "t_epoch", default: Some(std::f64::NAN) },
 
-    // Fixed observation time - ignore fourth coordinate.
+    // Fixed observation time - ignore the fourth coordinate.
     OpParameter::Real { key: "t_obs", default: Some(std::f64::NAN) },
 ];
 
@@ -242,7 +199,7 @@ pub fn new(parameters: &RawParameters, _provider: &dyn Provider) -> Result<Op, E
         (drz / 3600.).to_radians(),
     ];
 
-    // Handling of rotations
+    // Handling of rotations: position vector vs. coordinate frame conventions.
     let convention = params.text("convention")?;
     let rotated = !(R == [0., 0., 0.] && DR == [0., 0., 0.]);
     let mut position_vector = true;
@@ -259,38 +216,35 @@ pub fn new(parameters: &RawParameters, _provider: &dyn Provider) -> Result<Op, E
         params.boolean.insert("position_vector");
     }
 
-    // Scale and its time evoution
+    // Scale and its time evolution
     let mut S = 1.0 + params.real("s")? * 1e-6;
     let DS = params.real("ds")? * 1e-6;
 
     let dynamic = !(DT == [0., 0., 0.] && DR == [0., 0., 0.] && DS == 0.);
-    let mut epoch = 0.;
     if dynamic {
         params.boolean.insert("dynamic");
+
         // Check that epoch - "beginning of time for this transformation" is given
-        epoch = params.real("t_epoch")?;
+        let epoch = params.real("t_epoch")?;
         if epoch.is_nan() {
             return Err(Error::MissingParam("t_epoch".to_string()));
         }
-    }
 
-    // Fixed observation time - ignore the fourth coordinate.
-    let mut t_obs = 0.;
-    if let Ok(t) = params.real("t_obs") {
-        if !t.is_nan() {
-            t_obs = t;
-            params.boolean.insert("fixed_time");
+        // Fixed observation time - ignore the fourth coordinate and just compute
+        // the transformation matrix once
+        if let Ok(t_obs) = params.real("t_obs") {
+            if !t_obs.is_nan() {
+                params.boolean.insert("fixed_time");
+                for i in 0..3_usize {
+                    T[i] += DT[i] * (t_obs - epoch);
+                    R[i] += DR[i] * (t_obs - epoch);
+                    S += DS * (t_obs - epoch);
+                }
+            }
         }
     }
 
     let exact = params.boolean("exact");
-    if params.boolean("fixed_time") {
-        for i in 0..3_usize {
-            T[i] += DT[i] * (t_obs - epoch);
-            R[i] += DR[i] * (t_obs - epoch);
-            S += DS * (t_obs - epoch);
-        }
-    }
     params.series.insert("T", Vec::from(T));
     params.series.insert("DT", Vec::from(DT));
     params.series.insert("R", Vec::from(R));
@@ -299,8 +253,8 @@ pub fn new(parameters: &RawParameters, _provider: &dyn Provider) -> Result<Op, E
     params.real.insert("DS", DS);
 
     // The rotation matrix is a 3x3 symmetric matrix
-    let ROT = rotation_matrix(R.to_vec(), exact, position_vector);
-    dbg!(ROT);
+    let ROT = rotation_matrix(&R, exact, position_vector);
+
     // We need to turn the 3x3 into 1x9 to make it fit into the "series" store
     let mut ROTFLAT = Vec::from(ROT[0]);
     ROTFLAT.extend(ROT[1].iter());
@@ -322,7 +276,7 @@ pub fn new(parameters: &RawParameters, _provider: &dyn Provider) -> Result<Op, E
 // ----- R O T A T I O N   M A T R I X   B U I L D E R ---------------------------------
 
 // Based on Karsten Engsager's implementation in set_dtm_1.c (trlib),
-// but adds optional small angle approximation, and selection between
+// but adds optional small-angle approximation, and selection between
 // the "position vector" and "coordinate frame" rotation conventions.
 //
 // TO' = scale * [ROTZ * ROTY * ROTX] * FROM' + [translation x, y, z]'
@@ -331,16 +285,21 @@ pub fn new(parameters: &RawParameters, _provider: &dyn Provider) -> Result<Op, E
 // ROTZ = |-sz cz 0 |,   ROTY = | 0  1   0 |,   ROTX = | 0  cx sx |
 //        |  0  0 1 |           | sy 0  cy |           | 0 -sx cx |
 //
-fn rotation_matrix(r: Vec<f64>, exact: bool, position_vector: bool) -> [[f64; 3]; 3] {
+fn rotation_matrix(r: &[f64], exact: bool, position_vector: bool) -> [[f64; 3]; 3] {
     let (rx, ry, rz) = (r[0], r[1], r[2]);
 
-    // Small angle approximations: sx = sin(rx) = rx,  cx = cos(rx) = 1,  etc.
+    // Small-angle approximations: sx = sin(rx) = rx,  cx = cos(rx) = 1,  etc.
     let (mut sx, mut sy, mut sz) = (rx, ry, rz);
     let (mut cx, mut cy, mut cz) = (1.0, 1.0, 1.0);
 
-    // Destructuring assignments are still unstable - otherwise this if-block
-    // would be much more concise
+    // Leave out the second order infinitesimals in the rotation
+    // matrix elements, when using small-angle approximations
     if exact {
+        // Rust destructuring assignments are still unstable - otherwise
+        // this entire if-block would be a concise 3 line incantation:
+        //    let (sx, cx) = rx.sin_cos();
+        //    let (sy, cy) = ry.sin_cos();
+        //    let (sz, cz) = rz.sin_cos();
         let scx = rx.sin_cos();
         let scy = ry.sin_cos();
         let scz = rz.sin_cos();
@@ -354,8 +313,6 @@ fn rotation_matrix(r: Vec<f64>, exact: bool, position_vector: bool) -> [[f64; 3]
         cz = scz.1;
     }
 
-    // Leave out the second order infinitesimals in the rotation
-    // matrix elements, when using small angle approximations
     let r11 = cy * cz;
     let mut r12 = cx * sz;
     let mut r13 = -cx * sy * cz;
@@ -388,13 +345,23 @@ fn rotation_matrix(r: Vec<f64>, exact: bool, position_vector: bool) -> [[f64; 3]
 #[cfg(test)]
 mod tests {
     use super::*;
+    const GDA94: CoordinateTuple =
+        CoordinateTuple([-4052051.7643, 4212836.2017, -2545106.0245, 0.0]);
+    const GDA2020A: CoordinateTuple =
+        CoordinateTuple([-4052052.7379, 4212835.9897, -2545104.5898, 0.0]);
+    const GDA2020B: CoordinateTuple =
+        CoordinateTuple([-4052052.7373, 4212835.9835, -2545104.5867, 2020.0]);
+    const ITRF2014: CoordinateTuple =
+        CoordinateTuple([-4052052.6588, 4212835.9938, -2545104.6946, 2018.0]);
+
     #[test]
-    fn helmert() -> Result<(), Error> {
+    fn translation() -> Result<(), Error> {
         let provider = Minimal::default();
         let op = Op::new("helmert x=-87 y=-96 z=-120", &provider)?;
 
         // EPSG:1134 - 3 parameter, ED50/WGS84, s = sqrt(27) m
         let mut operands = [CoordinateTuple::origin()];
+
         op.apply(&provider, &mut operands, Direction::Fwd);
         assert_eq!(operands[0].first(), -87.);
         assert_eq!(operands[0].second(), -96.);
@@ -404,13 +371,16 @@ mod tests {
         assert_eq!(operands[0].first(), 0.);
         assert_eq!(operands[0].second(), 0.);
         assert_eq!(operands[0].third(), 0.);
+        Ok(())
+    }
 
-        // ---------------------------------------------------------------------------
-        // Test case from "Intergovernmental Committee on Surveying and Mapping (ICSM)
-        // Permanent Committee on Geodesy (PCG)": Geocentric Datum of Australia 2020,
-        // Technical Manual Version 1.0, 25 July 2017.
-        // Transformation from GDA94 to GDA2020.
-        // ---------------------------------------------------------------------------
+    // Test case from "Intergovernmental Committee on Surveying and Mapping (ICSM)
+    // Permanent Committee on Geodesy (PCG)": Geocentric Datum of Australia 2020,
+    // Technical Manual Version 1.0, 25 July 2017.
+    // Transformation from GDA94 to GDA2020.
+    #[test]
+    fn translation_rotation_and_scale() -> Result<(), Error> {
+        let provider = Minimal::default();
         let definition = "
             helmert convention = coordinate_frame
             x =  0.06155  rx = -0.0394924
@@ -418,71 +388,61 @@ mod tests {
             z = -0.04019  rz = -0.0328979
             s = -0.009994 exact
         ";
-
         let op = Op::new(definition, &provider)?;
-        let GDA94 = CoordinateTuple([-4052051.7643, 4212836.2017, -2545106.0245, 0.0]);
-        let GDA2020 = CoordinateTuple([-4052052.7379, 4212835.9897, -2545104.5898, 0.0]);
 
         // The forward transformation should hit closer than 75 um
         let mut operands = [GDA94];
         op.apply(&provider, &mut operands, Direction::Fwd);
-        assert!(GDA2020.hypot3(&operands[0]) < 75e-6);
+        assert!(GDA2020A.hypot3(&operands[0]) < 75e-6);
 
-        // ... and even closer on the way back
+        // ... and an even better roundtrip
         op.apply(&provider, &mut operands, Direction::Inv);
         assert!(GDA94.hypot3(&operands[0]) < 75e-7);
 
-        // ---------------------------------------------------------------------------
-        // And a time varying example from the same source: ITRF2014@2018 to GDA2020,
-        // Test point ALIC (Alice Springs)
-        // ---------------------------------------------------------------------------
-        // let full_definition = "
-        //     helmert
-        //     convention = coordinate_frame
-        //     x = 0  rx = 0   dx = 0   drx = 0.00150379
-        //     y = 0  ry = 0   dy = 0   dry = 0.00118346
-        //     z = 0  rz = 0   dz = 0   drz = 0.00120716
-        //     s = 0  ds = 0
-        //     t_epoch = 2020.0
-        //     exact
-        // ";
+        Ok(())
+    }
+
+    // A time varying example from the same source: ITRF2014@2018 to GDA2020,
+    // Test point ALIC (Alice Springs)
+    #[test]
+    fn dynamic() -> Result<(), Error> {
+        let provider = Minimal::default();
         let definition = "
             helmert  exact    convention = coordinate_frame
             drx = 0.00150379  dry = 0.00118346  drz = 0.00120716
             t_epoch = 2020.0
         ";
         let op = Op::new(definition, &provider)?;
-        let ITRF2014 = CoordinateTuple([-4052052.6588, 4212835.9938, -2545104.6946, 2018.0]);
-        let GDA2020 = CoordinateTuple([-4052052.7373, 4212835.9835, -2545104.5867, 2020.0]);
 
         // The forward transformation should hit closeer than 40 um
         let mut operands = [ITRF2014];
         op.apply(&provider, &mut operands, Direction::Fwd);
-        assert!(GDA2020.hypot3(&operands[0]) < 40e-6);
+        assert!(dbg!(GDA2020B.hypot3(&operands[0])) < 40e-6);
 
         // ... and even closer on the way back
         op.apply(&provider, &mut operands, Direction::Inv);
-        assert!(ITRF2014.hypot3(&operands[0]) < 40e-8);
+        assert!(dbg!(ITRF2014.hypot3(&operands[0])) < 40e-8);
 
+        Ok(())
+    }
 
-        /* TODO: t_obs fails
+    // Same as above, but with fixed time `t_obs` option
+    #[test]
+    fn fixed_dynamic() -> Result<(), Error> {
+        let provider = Minimal::default();
         let definition = "
             helmert  exact    convention = coordinate_frame
             drx = 0.00150379  dry = 0.00118346  drz = 0.00120716
             t_epoch = 2020.0  t_obs = 2018
         ";
         let mut operands = [ITRF2014];
-        //operands[0][3] = 2030.;
+        operands[0][3] = 2030.;
+
         let op = Op::new(definition, &provider)?;
         op.apply(&provider, &mut operands, Direction::Fwd);
-        dbg!(operands);
-        dbg!(GDA2020);
-        assert!(GDA2020.hypot3(&operands[0]) < 40e-6);
-
-        // ... and even closer on the way back
+        assert!(GDA2020B.hypot3(&operands[0]) < 40e-6);
         op.apply(&provider, &mut operands, Direction::Inv);
         assert!(ITRF2014.hypot3(&operands[0]) < 40e-8);
-        */
 
         Ok(())
     }
