775e9bb6dd3b5f1c7ad9f530cc5449324bd14469
Thomas Knudsen <knudsen.thomas@gmail.com>
2023-01-22 14:33:09 +0100

    LAEA forward direction completed

--- src/inner_op/laea.rs
+++ src/inner_op/laea.rs
@@ -1,4 +1,5 @@
-//! Lambert azimuthal equal area: EPSG method
+//! Lambert azimuthal equal area: EPSG coordinate operation method 9820, implemented
+//! following the IOGP Geomatics Guidance Note Number 7, part 2, pp.78--80
 use super::*;
 use std::f64::consts::FRAC_PI_2;
 const EPS10: f64 = 1e-10;
@@ -8,9 +9,38 @@ const EPS10: f64 = 1e-10;
 // ----- F O R W A R D -----------------------------------------------------------------
 
 fn fwd(op: &Op, _ctx: &dyn Context, operands: &mut [Coord]) -> Result<usize, Error> {
-    todo!();
+    let Ok(xi_0) = op.params.real("xi_0") else { return Ok(0) };
+    let Ok(qp)   = op.params.real("qp")   else { return Ok(0) };
+    let Ok(rq)   = op.params.real("rq")   else { return Ok(0) };
+    let Ok(d)    = op.params.real("d")    else { return Ok(0) };
+
+    let lon_0 = op.params.lon(0);
+    let x_0 = op.params.x(0);
+    let y_0 = op.params.y(0);
+    let ellps = op.params.ellps(0);
+    let e = ellps.eccentricity();
+
+    let (sin_xi_0, cos_xi_0) = xi_0.sin_cos();
+
     let mut successes = 0_usize;
     for coord in operands {
+        let lon = coord[0];
+        let lat = coord[1];
+        let (sin_lon, cos_lon) = (lon - lon_0).sin_cos();
+
+        // Authalic latitude, 𝜉
+        let xi = (qs(lat.sin(), e) / qp).asin();
+
+        let (sin_xi, cos_xi) = xi.sin_cos();
+        let factor = 1.0 + sin_xi_0 * sin_xi + (cos_xi_0 * cos_xi * cos_lon);
+        let b = rq * (2.0 / factor).sqrt();
+
+        // Easting
+        coord[0] = x_0 + (b * d) * (cos_xi * sin_lon);
+
+        // Northing
+        coord[1] = y_0 + (b / d) * (cos_xi_0 * sin_xi - sin_xi_0 * cos_xi * cos_lon);
+
         successes += 1;
     }
 
@@ -20,9 +50,8 @@ fn fwd(op: &Op, _ctx: &dyn Context, operands: &mut [Coord]) -> Result<usize, Err
 // ----- I N V E R S E -----------------------------------------------------------------
 
 fn inv(op: &Op, _ctx: &dyn Context, operands: &mut [Coord]) -> Result<usize, Error> {
-    todo!();
     let mut successes = 0_usize;
-    for coord in operands {
+    for _coord in operands {
         successes += 1;
     }
 
@@ -48,7 +77,7 @@ pub fn new(parameters: &RawParameters, _ctx: &dyn Context) -> Result<Op, Error> 
     let def = &parameters.definition;
     let mut params = ParsedParameters::new(parameters, &GAMUT)?;
 
-    let mut lat_0 = params.lat[0];
+    let lat_0 = params.lat[0];
     if lat_0.is_nan() {
         warn!("LAEA: Bad central latitude!");
         return Err(Error::BadParam("lat_0".to_string(), def.clone()));
@@ -71,13 +100,32 @@ pub fn new(parameters: &RawParameters, _ctx: &dyn Context) -> Result<Op, Error> 
         _ => params.boolean.insert("oblique"),
     };
 
-    let sc = lat_0.sin_cos();
-    let mut n = sc.0;
+    // --- Precompute some latitude invariant factors ---
+
+    let a = params.ellps[0].semimajor_axis();
     let es = params.ellps[0].eccentricity_squared();
     let e = es.sqrt();
-    let qp = qs(sc.0, e);
+    let (sin_phi_0, cos_phi_0) = lat_0.sin_cos();
+
+    // qs for the central parallel
+    let q0 = qs(sin_phi_0, e);
+    // qs for the North Pole
+    let qp = qs(1.0, e);
+    // Authalic latitude of the central parallel - 𝛽₀ in the IOGP text
+    let xi_0 = (q0 / qp).asin();
+    // Rq in the IOGP text
+    let rq = a * (0.5 * qp).sqrt();
+    // D in the IOGP text
+    let d = a * (cos_phi_0 / (1.0 - es * sin_phi_0 * sin_phi_0).sqrt()) / (rq * xi_0.cos());
+
+    params.real.insert("xi_0", xi_0);
+    params.real.insert("q0", q0);
+    params.real.insert("qp", qp);
+    params.real.insert("rq", rq);
+    params.real.insert("d", d);
 
     /*
+    PROJ snippet:
     Q->mmf = .5 / (1. - P->es);
     Q->apa = pj_authset(P->es);
     if (nullptr==Q->apa)
@@ -106,9 +154,15 @@ pub fn new(parameters: &RawParameters, _ctx: &dyn Context) -> Result<Op, Error> 
     P->inv = laea_e_inverse;
     P->fwd = laea_e_forward;
     */
-
-    //Ok(Op::default());
-    Err(Error::General("Failure"))
+    let descriptor = OpDescriptor::new(def, InnerOp(fwd), Some(InnerOp(inv)));
+    let steps = Vec::<Op>::new();
+    let id = OpHandle::new();
+    Ok(Op {
+        descriptor,
+        params,
+        steps,
+        id,
+    })
 }
 
 // ----- A N C I L L A R Y   F U N C T I O N S -----------------------------------------
@@ -120,24 +174,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn template() -> Result<(), Error> {
+    fn laea_oblique() -> Result<(), Error> {
         let mut ctx = Minimal::default();
-        // EPSG:1134 - 3 parameter, ED50/WGS84, s = sqrt(27) m
-        let op = ctx.op("helmert x=-87 y=-96 z=-120")?;
+        let op = ctx.op("laea ellps=GRS80 lat_0=52 lon_0=10  x_0=4321000 y_0=3210000")?;
 
-        let mut operands = [Coord::origin()];
+        let mut operands = [Coord::origin(), Coord::geo(50.0, 5.0, 0.0, 0.0)];
 
         // Forward
         ctx.apply(op, Fwd, &mut operands)?;
-        assert_eq!(operands[0].first(), -87.);
-        assert_eq!(operands[0].second(), -96.);
-        assert_eq!(operands[0].third(), -120.);
+        println!("{:#?}", operands);
+        assert!(1 == 2);
 
-        // Inverse + roundtrip
-        ctx.apply(op, Inv, &mut operands)?;
-        assert_eq!(operands[0].first(), 0.);
-        assert_eq!(operands[0].second(), 0.);
-        assert_eq!(operands[0].third(), 0.);
+        // assert_eq!(operands[0].first(), -87.);
+        // assert_eq!(operands[0].second(), -96.);
+        // assert_eq!(operands[0].third(), -120.);
+        // // Inverse + roundtrip
+        // ctx.apply(op, Inv, &mut operands)?;
+        // assert_eq!(operands[0].first(), 0.);
+        // assert_eq!(operands[0].second(), 0.);
+        // assert_eq!(operands[0].third(), 0.);
         Ok(())
     }
 }
