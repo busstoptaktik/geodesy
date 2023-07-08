0db988e6e4ae2f9d8bb8b631e6bb3e954af5818a
Thomas Knudsen <knudsen.thomas@gmail.com>
2023-07-08 12:30:38 +0200

    Switch examples from simple-logger to env-logger

--- examples/06-user_defined_coordinate_types_and_containers.rs
+++ examples/06-user_defined_coordinate_types_and_containers.rs
@@ -78,12 +78,7 @@ impl CoordinateSet for AbscissaCollection {
 }
 
 fn main() -> Result<(), anyhow::Error> {
-    // Filter by setting RUST_LOG to one of {Error, Warn, Info, Debug, Trace}
-    if std::env::var("RUST_LOG").is_err() {
-        simple_logger::init_with_level(log::Level::Error)?;
-    } else {
-        simple_logger::init_with_env()?;
-    }
+    env_logger::init();
 
     debug!("User defined coordinate types and containers");
 
