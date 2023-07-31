6bafb7df2c2a965b4ed97357c6a709e6c6e70eda
Thomas Knudsen <knudsen.thomas@gmail.com>
2023-07-31 18:54:17 +0200

    Introducing the jacobian

--- src/context/minimal.rs
+++ src/context/minimal.rs
@@ -121,6 +121,94 @@ impl Context for Minimal {
     }
 }
 
+
+impl Minimal {
+    /// Returns a `Coor4D`, where the elements represent (dx/dλ, dy/dφ, dx/dφ, dy/dλ) (x_l, y_p, x_p, y_l)
+    /// Mostly based on the PROJ function [pj_deriv](https://github.com/OSGeo/PROJ/blob/master/src/deriv.cpp),
+    /// with appropriate adaptations to the fact that PROJ internally sets the semimajor axis, a = 1
+    fn jacobian(
+        &self,
+        op: OpHandle,
+        operands: &dyn CoordinateSet,
+    ) -> Result<(Ellipsoid, Vec<Coor4D>), Error> {
+        const BAD_ID_MESSAGE: Error = Error::General("Minimal: Unknown operator id");
+        let op = self.operators.get(&op).ok_or(BAD_ID_MESSAGE)?;
+
+        let ellps = op.params.ellps(0);
+
+        let h = 1e-8;
+
+        let mut j = Vec::with_capacity(operands.len());
+        let mut coo = Vec::with_capacity(operands.len());
+
+        // North-east of POI
+        for i in 0..operands.len() {
+            let coord = operands.get_coord(i);
+            coo.push(Coor2D::raw(coord[0] + h, coord[1] + h));
+        }
+        op.apply(self, &mut coo, Fwd);
+
+        for i in 0..operands.len() {
+            let coord = coo[i];
+            j.push(Coor4D::raw(coord[0], coord[1], coord[0], coord[1]));
+        }
+
+        // South-east of POI
+        for i in 0..operands.len() {
+            let coord = operands.get_coord(i);
+            coo[i] = Coor2D::raw(coord[0] + h, coord[1] - h);
+        }
+        op.apply(self, &mut coo, Fwd);
+
+        for i in 0..operands.len() {
+            let x = coo[i][0];
+            let y = coo[i][1];
+            let ji = j[i];
+            j[i] = Coor4D::raw(ji[0] + x, ji[1] - y, ji[2] - x, ji[3] + y);
+        }
+
+
+        // South-west of POI
+        for i in 0..operands.len() {
+            let coord = operands.get_coord(i);
+            coo[i] = Coor2D::raw(coord[0] - h, coord[1] - h);
+        }
+        op.apply(self, &mut coo, Fwd);
+
+        for i in 0..operands.len() {
+            let x = coo[i][0];
+            let y = coo[i][1];
+            let ji = j[i];
+            j[i] = Coor4D::raw(ji[0] - x, ji[1] - y, ji[2] - x, ji[3] - y);
+        }
+
+        // North-west of POI
+        for i in 0..operands.len() {
+            let coord = operands.get_coord(i);
+            coo[i] = Coor2D::raw(coord[0] - h, coord[1] + h);
+        }
+        op.apply(self, &mut coo, Fwd);
+
+        for i in 0..operands.len() {
+            let x = coo[i][0];
+            let y = coo[i][1];
+            let ji = j[i];
+            j[i] = Coor4D::raw(ji[0] - x, ji[1] + y, ji[2] + x, ji[3] - y);
+        }
+
+        // Normalize
+        for i in 0..operands.len() {
+            let d = 4.0 * h * ellps.semimajor_axis();
+            j[i][0] /= d;
+            j[i][1] /= d;
+            j[i][2] /= d;
+            j[i][3] /= d;
+        }
+
+        Ok((*ellps, j))
+    }
+}
+
 // ----- T E S T S ------------------------------------------------------------------
 
 #[cfg(test)]
@@ -202,6 +290,51 @@ mod tests {
         // Taking a look at the internals is not too hard either
         // let params = ctx.params(op, 0)?;
         // dbg!(params);
+
+        Ok(())
+    }
+
+    #[test]
+    fn jacobian() -> Result<(), Error> {
+        let mut ctx = Minimal::new();
+
+        let op = ctx.op("utm zone=32")?;
+
+        let mut data = [
+            Coor2D::geo(55., 12.),
+            Coor2D::geo(59., 18.)
+        ];
+
+        // (dx/dλ, dy/dφ, dx/dφ, dy/dλ) (x_l, y_p, x_p, y_l)
+        let (ellps, jac) = ctx.jacobian(op, &mut data)?;
+        dbg!(&jac);
+
+        let es = ellps.eccentricity_squared();
+
+        let cosphi = data[0][1].cos();
+        let x_l = jac[0][0];
+        let y_p = jac[0][1];
+        let x_p = jac[0][2];
+        let y_l = jac[0][3];
+
+        let h = x_p.hypot(y_p);
+        dbg!(h);
+        let k = x_l.hypot(y_l) / cosphi;
+        dbg!(k);
+
+        let t = data[0][1].sin();
+        let t = 1. - es * t * t;
+        let n = t.sqrt();
+        let h = h * (t * n / (1. - es));
+        let k = k * n;
+        let r = t * t / (1. - es);
+        dbg!(h);
+        dbg!(k);
+
+
+        let conv = -jac[0][2].atan2(jac[0][1]).to_degrees();
+        dbg!(conv);
+        assert_eq!(h, 3.45);
 
         Ok(())
     }
