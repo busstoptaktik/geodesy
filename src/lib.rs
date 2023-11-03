f003ec03f50a2f9f0c6a357e859082d1a03ef222
Sean Rennie <sean.rennie@sensat.co.uk>
2023-11-03 10:59:29 +0000

    merge fixes

--- src/lib.rs
+++ src/lib.rs
@@ -50,9 +50,9 @@ pub mod prelude {
 pub mod authoring {
     pub use crate::prelude::*;
 
+    pub use crate::grid::BaseGrid;
     pub use crate::grid::Grid;
     pub use crate::math::*;
-    pub use crate::GridTrait;
     pub use crate::InnerOp;
     pub use crate::Op;
     pub use crate::OpConstructor;
@@ -188,7 +188,7 @@ pub use crate::token::Tokenize;
 pub use crate::token::parse_proj;
 
 // The lower level data types, mostly use in the extended prelude 'authoring'
-pub use crate::grid::GridTrait;
+pub use crate::grid::Grid;
 pub use crate::inner_op::InnerOp;
 pub use crate::inner_op::OpConstructor;
 pub use crate::op::Op;
