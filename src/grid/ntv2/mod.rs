1ef3cf29c9d5ef9032a2f66539b2f45b8feb424d
Sean Rennie <sean.rennie@sensat.co.uk>
2023-11-03 11:44:20 +0000

    use within when checking a coord is contained in a grid

--- src/grid/ntv2/mod.rs
+++ src/grid/ntv2/mod.rs
@@ -1,5 +1,5 @@
 use self::parser::{parse_subgrid_grid, parse_subgrid_header, HEADER_SIZE};
-use crate::{Coor2D, Coor4D, Error, GridTrait};
+use crate::{Coor2D, Coor4D, Error, Grid};
 use parser::NTv2Parser;
 mod parser;
 
@@ -66,21 +66,36 @@ impl Ntv2Grid {
     }
 }
 
-impl GridTrait for Ntv2Grid {
+impl Grid for Ntv2Grid {
     fn bands(&self) -> usize {
         2
     }
 
-    /// Checks if a `Coord4D` is within the grid limits
-    fn contains(&self, position: Coor4D) -> bool {
+    /// Checks if a `Coord4D` is within the grid limits +- `within` grid units
+    fn contains(&self, position: &Coor4D, _within: f64) -> bool {
         let lon = position[0];
         let lat = position[1];
-        lat >= self.slat && lat <= self.nlat && lon >= self.wlon && lon <= self.elon
+
+        let grace = _within * self.dlat;
+        if lat != lat.clamp(self.slat - grace, self.nlat + grace) {
+            return false;
+        }
+
+        let grace = _within * self.dlon;
+        if lon != lon.clamp(self.wlon - grace, self.elon + grace) {
+            return false;
+        }
+
+        // If we fall through to here we're within the grid
+        true
     }
 
-    // Matches the `interpolation` method signature of the `Grid` struct
     /// Implementation adapted from [projrs](https://github.com/3liz/proj4rs/blob/8b5eb762c6be65eed0ca0baea33f8c70d1cd56cb/src/nadgrids/grid.rs#L206C1-L252C6) && [proj4js](https://github.com/proj4js/proj4js/blob/d9faf9f93ebeccac4b79fa80f3e9ad8a7032828b/lib/datum_transform.js#L167)
-    fn interpolation(&self, coord: &Coor4D, _grid: Option<&[f32]>) -> Coor4D {
+    fn interpolation(&self, coord: &Coor4D, within: f64) -> Option<Coor4D> {
+        if !self.contains(coord, within) {
+            return None;
+        }
+
         // Normalise to the grid origin which is the SW corner
         let rlon = coord[0] - self.wlon;
         let rlat = coord[1] - self.slat;
@@ -88,14 +103,6 @@ impl GridTrait for Ntv2Grid {
         let (t_lon, t_lat) = (rlon / self.dlon, rlat / self.dlat);
         let (i_lon, i_lat) = (t_lon.floor(), t_lat.floor());
         let (f_lon, f_lat) = (t_lon - 1.0 * i_lon, t_lat - 1.0 * i_lat);
-
-        if i_lon < 0. || i_lon >= self.row_size {
-            return Coor4D::raw(f64::NAN, f64::NAN, 0., 0.);
-        }
-
-        if i_lat < 0. || i_lat >= self.num_rows {
-            return Coor4D::raw(f64::NAN, f64::NAN, 0., 0.);
-        }
 
         let mut index = (i_lat * self.row_size + i_lon) as usize;
         let f00 = &self.grid[index];
@@ -113,6 +120,6 @@ impl GridTrait for Ntv2Grid {
         result[0] = -(m00 * f00[0] + m10 * f10[0] + m01 * f01[0] + m11 * f11[0]); // lon
         result[1] = m00 * f00[1] + m10 * f10[1] + m01 * f01[1] + m11 * f11[1]; // lat
 
-        result
+        Some(result)
     }
 }
